219159e3257895c54178ae13f7ef2751dc797c0c
sahuvicky1993 <vivekkumarsahu@yahoo.com>
2018-06-12 18:48:42 +0530

    Create iteratorINCREAMENT.cpp
    Program crashes while writing "++it" out side the for loop

---
+++ iteratorINCREAMENT.cpp
@@ -0,0 +1,22 @@
+void store(vector<string>s ){
+    vector<string>::iterator it=s.begin();
+   //printVector(s);
+   string value,name,tag;
+   tag=*it;
+   //it++;
+   cout<<tag<<endl;
+   if(myCompare(*it,"value"))
+   {
+     //  it++;
+   //value=*it;
+   }
+   else
+   value=*it;
+   
+   /*if(myCompare(*it,"name"))
+   name=*it;
+   else 
+   name=*it;*/
+  // m.insert( make_pair(tag, make_pair(value,name)) );
+   
+}
